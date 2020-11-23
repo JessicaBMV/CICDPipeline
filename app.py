dfcfc62975e9fba3d9547b8fd06b0e5985f52676
JessicaBMV <jess.menesses11@gmail.com>
2020-11-23 22:59:16 +0100

    Added tests

--- app.py
+++ app.py
@@ -6,67 +6,71 @@ import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
-LOG = create_logger(app)
-LOG.setLevel(logging.INFO)
+def create_app(test_config=None):
+    # create and configure the app
+  app = Flask(__name__)
+  LOG = create_logger(app)
+  LOG.setLevel(logging.INFO)
 
-def scale(payload):
-    """Scales Payload"""
+  
 
-    LOG.info("Scaling Payload: %s payload")
-    scaler = StandardScaler().fit(payload)
-    scaled_adhoc_predict = scaler.transform(payload)
-    return scaled_adhoc_predict
+  def scale(payload):
+      """Scales Payload"""
 
-@app.route("/")
-def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+      LOG.info("Scaling Payload: %s payload")
+      scaler = StandardScaler().fit(payload)
+      scaled_adhoc_predict = scaler.transform(payload)
+      return scaled_adhoc_predict
 
-# TO DO:  Log out the prediction value
-@app.route("/predict", methods=['POST'])
-def predict():
-    """Performs an sklearn prediction
+  @app.route("/")
+  def home():
+      html = "<h3>Sklearn Prediction Home</h3>"
+      return html.format(format)
 
-    input looks like:
-            {
-    "CHAS":{
-      "0":0
-    },
-    "RM":{
-      "0":6.575
-    },
-    "TAX":{
-      "0":296.0
-    },
-    "PTRATIO":{
-       "0":15.3
-    },
-    "B":{
-       "0":396.9
-    },
-    "LSTAT":{
-       "0":4.98
-    }
+  # TO DO:  Log out the prediction value
+  @app.route("/predict", methods=['POST'])
+  def predict():
+      """Performs an sklearn prediction
 
-    result looks like:
-    { "prediction": [ 20.35373177134412 ] }
+      input looks like:
+              {
+      "CHAS":{
+        "0":0
+      },
+      "RM":{
+        "0":6.575
+      },
+      "TAX":{
+        "0":296.0
+      },
+      "PTRATIO":{
+        "0":15.3
+      },
+      "B":{
+        "0":396.9
+      },
+      "LSTAT":{
+        "0":4.98
+      }
 
-    """
+      result looks like:
+      { "prediction": [ 20.35373177134412 ] }
 
-    try:
-        clf = joblib.load("boston_housing_prediction.joblib")
-    except:
-        LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
+      """
 
-    json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
-    scaled_payload = scale(inference_payload)
-    prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction})
+      try:
+          clf = joblib.load("boston_housing_prediction.joblib")
+      except:
+          LOG.info("JSON payload: %s json_payload")
+          return "Model not loaded"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+      json_payload = request.json
+      LOG.info("JSON payload: %s json_payload")
+      inference_payload = pd.DataFrame(json_payload)
+      LOG.info("inference payload DataFrame: %s inference_payload")
+      scaled_payload = scale(inference_payload)
+      prediction = list(clf.predict(scaled_payload))
+      return jsonify({'prediction': prediction})
+    
+  return app
+
--- tests/tests.py
+++ tests/tests.py
@@ -1,22 +1,72 @@
 import sys
 from pathlib import Path
+import unittest
 sys.path.append(str(Path('.').absolute().parent))
 from hello import toyou, add, subtract
+from app import create_app
+from flask import Flask
+import requests
+import json
 
 
-def setup_function(function):
-    print("Running Setup: %s" % function.__name__)
-    function.x = 10
+class TestPurchaseRequest(unittest.TestCase):
+    # def setup_function(function):
+    #     print("Running Setup: %s" % function.__name__)
+    #     function.x = 10
 
 
-def teardown_function(function):
-    print("Running Teardown: %s" % function.__name__)
-    del function.x
+    # def teardown_function(function):
+    #     print("Running Teardown: %s" % function.__name__)
+    #     del function.x
 
 
-### Run to see failed test
-#def test_hello_add():
-#    assert add(test_hello_add.x) == 12
+    # ### Run to see failed test
+    # #def test_hello_add():
+    # #    assert add(test_hello_add.x) == 12
 
-def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+    # def test_hello_subtract():
+    #     assert subtract(test_hello_subtract.x) == 9
+
+    def setUp(self):
+        self.app = create_app()
+        self.app_client = self.app.test_client()
+
+    def test_home(self):
+        print(self.app_client)
+        response =  self.app_client.get("/")
+        self.assertEqual(response.status_code, 200)
+        
+
+    def test_predict(self):
+        
+        mimetype = 'application/json'
+        headers = {
+            'Content-Type': mimetype,
+            'Accept': mimetype
+        }
+        data = {
+                "CHAS":{
+                "0":0
+                },
+                "RM":{
+                "0":6.575
+                },
+                "TAX":{
+                "0":296.0
+                },
+                "PTRATIO":{
+                "0":15.3
+                },
+                "B":{
+                "0":396.9
+                },
+                "LSTAT":{
+                "0":4.98
+                }
+            }
+        response = self.app_client.post('/predict',
+                                 data=json.dumps(data),
+                                 content_type='application/json',  headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.data)
+        self.assertEqual(response.data , "{ 'prediction': [ 20.35373177134412 ] }")
